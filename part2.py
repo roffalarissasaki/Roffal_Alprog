463125d5c656770ad3d0f405a9e883362c85da83
roffalarissasaki <roffalsasaki23@gmail.com>
2025-11-09 13:44:11 +0700

    Add files via upload

---
+++ part2.py
@@ -0,0 +1,27 @@
+# belajar variabel
+# cara kita menyimpan nilai 
+
+# memberikan nilai (assigment)
+x = 10
+y = 6
+lebar = 13
+
+print(x + y + lebar)
+print("nilai dari x =", x)
+print("nilai dari y =", y)
+print("nilai dari lebar =", lebar)
+
+# tings to consider when making variabel
+# hal yang harus di perhatikan ketika membuat variabel 
+
+bet99 = 9 # tidak boleh di awali dengan anka 
+jumlah_hutang = 1000000 # menggunakan dua susku kata harus di gabungkan dengan _
+JumlahHutang = 1000000 # menggunakan dua susku kata harus di gabungkan dengan huruf KAPITAL
+
+# pemanggilan indirect
+z = lebar
+print("nilai dari z =", z)
+
+# Tugas
+# nama saya adalah : (Roffal aris sasaki)
+# nim saya adalah : (25241105)
