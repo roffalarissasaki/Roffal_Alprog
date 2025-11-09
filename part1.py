04ef67a947a45a43948514aaedf6b3ca5d1c5337
roffalarissasaki <roffalsasaki23@gmail.com>
2025-11-09 13:42:48 +0700

    Add files via upload

---
+++ part1.py
@@ -0,0 +1,21 @@
+# saya sedang belajar python
+# code di baca dari kiri ke kanan
+# code di baca dari atas ke bawah
+
+# cara untuk memindahkan satu baris, arahkan krusor pada baris yg ingin di pindahkan
+# tahan tombol alt, lalu tekan tombol arah bawah atau atas
+
+# cara untuk mengcopy satu baris full, sama dengan yg di atas
+ #namun ditambahkan tombol shift
+
+print("nama saya roffal")
+print("saya adalah mahasiswa yang manis")
+print("tapi humoris")
+
+x = 9
+y = 6
+print("x + y")
+
+# Tugas 
+# nama saya adalah : (Roffal aris sasaki)
+# nim saya adalah : (25241105)
