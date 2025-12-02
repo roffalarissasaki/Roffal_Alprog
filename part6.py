0eac180ae55b47ed1443c443bab44acadf0efb89
Roffal aris sasaki <roffalsasaki23@gmail.com>
2025-12-03 01:32:47 +0800

    Add files via upload

---
+++ part6.py
@@ -0,0 +1,32 @@
+# program konversi suhu
+
+celcius = float(input(("masukan suhu anda :")))
+print("suhu anda dalam celcius:", celcius)
+
+#fahrenheit x 9/5 x celcius + 32
+fahrenheit = (9/5 * celcius) + 32
+print("suhu anda dalam fahrenheit :", fahrenheit)
+
+# reamur
+# kelvin
+
+fahrenheit = float(input(("masukan suhu anda:")))
+print("suhu anda dalam fahrenheit", fahrenheit)
+
+# celcius
+# reamur
+# kelvin
+
+reamur = float(input(("masukan suhu anda :")))
+print("suhu anda dalam reamur:", reamur)
+
+# celcius
+# fahrenheit
+# kelvin
+
+kelvin = float(input(("masukan suhu anda: ")))
+print("suhu anda dalam kelvin:", kelvin)
+
+# celcius
+# reamur
+# kelvin
